d4c0156adf941efc0e0256661f2d64aae07cd129
Petr Klika <pe.klika@gmail.com>
2021-10-25 20:24:35 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { EMPTY } from 'rxjs';
+import {
+  filter,
+  catchError,
+  share
+} from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+
+interface Quote {
+  quote: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'recipe-strapi-mongo-angular-gui';
+export class AppComponent implements OnInit {
+
+  quote: string;
+
+  constructor(private _httpClient: HttpClient) {}
+
+  ngOnInit() {
+    this._httpClient
+      .post<Quote>(
+        `${environment.apiUrl}/quotes`,
+        {
+          quote: 'Zerops has beautiful user interface :)'
+        }
+      )
+      .pipe(
+        catchError(() => EMPTY),
+        filter((d) => !!d),
+        share()
+      ).subscribe((d) => this.quote = d.quote);
+  }
+
 }
